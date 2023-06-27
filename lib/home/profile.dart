eccca2c9283c1acc61e605c9e8945818876b0a1b
raedbgr <raed.bouguerra@gmail.com>
2023-06-27 17:26:20 +0100

    - Bug Fixes

--- lib/home/profile.dart
+++ lib/home/profile.dart
@@ -1,19 +1,58 @@
+import 'package:auth_login/controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class Profile extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade300,
       appBar: AppBar(
         backgroundColor: Colors.grey.shade900,
         title: Text('Profile'),
+        centerTitle: true,
         leading: IconButton(
             onPressed: () {
               Get.back();
             },
             icon: Icon(Icons.arrow_back)
         ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Container(
+              height: 200,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                border: Border.all(color: Colors.grey.shade100, width: 2),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.account_circle, size: 100,),
+                      SizedBox(width: 15,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Username : example123'),
+                          SizedBox(height: 15,),
+                          Text('Email : example@gmail.com'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
